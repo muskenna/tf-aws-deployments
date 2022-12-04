ecd36793bad0fd1da0d9f729c18b8afe16f11d98
Paulo <paulo.leao@move.com>
2022-12-03 21:36:47 -0800

    q

--- utilities/setDeploymentServiceAccessControl.py
+++ utilities/setDeploymentServiceAccessControl.py
@@ -138,7 +138,7 @@ deploymentServiceConfiguration = deploymentConfiguration['deploymentService']
 localAWSCredentialsProfileName = deploymentServiceConfiguration['localAWSCredentialsProfileName']
 roleName = deploymentServiceConfiguration['roleName']
 
-localAWSCredentialsProfileName = "cloud_user"
+logging.info(f'localAWSCredentialsProfileName: {localAWSCredentialsProfileName}')
 deploymentServiceSession = boto3.session.Session(profile_name=localAWSCredentialsProfileName)
 iamClient = deploymentServiceSession.client('iam')
 identity = deploymentServiceSession.client('sts').get_caller_identity()
